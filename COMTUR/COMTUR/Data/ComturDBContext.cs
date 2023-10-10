865d6e3a74d51ce5cfb02abcfffd9eba7dc003df
heyyana <anacconceicao2005@gmail.com>
2023-10-10 09:31:19 -0300

    Adicionando TipoTurismo ao novo repositorio

---
+++ COMTUR/COMTUR/Data/ComturDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using COMTUR.Models;
+using COMTUR.Data.Map;
+
+namespace COMTUR.Data
+{
+	public class ComturDBContext : DbContext
+	{
+		public ComturDBContext(DbContextOptions<ComturDBContext> options) : base(options)
+		{
+		}
+
+		public DbSet<TipoTurismoModel> TipoTurismo { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfiguration(new TipoTurismoMap());
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}

4cfbbde393c7627e5c36e540d5ac658ce45d64a2
nabilasampaio2005 <nabilasampaio@gmail.com>
2024-03-20 11:18:52 -0300

    :sparkles: Adicionando a primeira tentativa de login

---
+++ COMTUR/COMTUR/Models/EntitySecurity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace COMTUR.Models
+{
+	public class EntitySecurity
+	{
+			public string Issuer { get; } = "Server API";
+			public string Audience { get; } = "WebSite";
+			public string Key { get; } = "COMTUR_BarramentUser_API_Autentication";
+		}
+}

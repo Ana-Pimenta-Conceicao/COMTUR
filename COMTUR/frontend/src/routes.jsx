6f5a8aca48516e9dd7e130a63d1f75bc04fa5fe1
GabrielaCS13 <gabriela.silva.00243@gmail.com>
2024-03-30 14:21:30 -0300

    Adicionando a tela de cadastro do usuario comum e de empresario

--- COMTUR/frontend/src/routes.jsx
+++ COMTUR/frontend/src/routes.jsx
@@ -15,6 +15,8 @@ import Usuario from "./screens/usuarios/usuarios";
 import Login from "./screens/login/login";
 import TipoAtracao from "./screens/tipoatracao/tipoatracao";
 import TodasNoticias from "./screens/noticia/todasNoticias";
+import CadastroUsuario from "./screens/cadastro/cadastrousuario";
+import CadastroEmpresario from "./screens/cadastro/cadastroEmpresario";
 
 
 
@@ -38,6 +40,8 @@ const AppRoutes = () => {
                 <Route path="/login" element={ <Login/> } />
                 <Route path="/administrador" element={ <Administrador/> } />
                 <Route path="/todasnoticias" element={<TodasNoticias/>} />
+                <Route path="/cadastrousuario" element={<CadastroUsuario/>} />
+                <Route path="/cadastroempresario" element={<CadastroEmpresario/>} />
             
             </Routes>
         </Router>

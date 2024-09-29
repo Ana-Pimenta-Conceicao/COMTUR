a6cfabac6862b284422059c6461fe5a7bfc67257
ThaysaVitoria <thaysavitoria@gmail.com>
2024-09-29 19:13:31 -0300

    :lipstick: feat: Estilização da tela home e ajustando menu user

--- COMTUR/frontend/src/components/user/navbarUsr.jsx
+++ COMTUR/frontend/src/components/user/navbarUsr.jsx
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Importando Font Awesome
 import { useNavigate } from "react-router-dom";
-import logoSF from '../../assets/logoComturSF.png';
-import './folhasyle.css';
+import logoSF from '../../assets/logoComturNovo.svg';
+
 
 const NavbarUsr = () => {
   const navigate = useNavigate();
@@ -13,12 +14,10 @@ const NavbarUsr = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 991);
 
   useEffect(() => {
-    // Adiciona um ouvinte de evento para fechar o menu ao clicar em um link
     $('.navbar-nav a').on('click', () => {
       setIsNavbarOpen(false);
     });
 
-    // Adiciona um ouvinte de evento para atualizar o estado de isMobile ao redimensionar a janela
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 991);
     };
@@ -39,35 +38,92 @@ const NavbarUsr = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-black sticky-top px-4 px-lg-5">
+    <>
+      <style>
+        {`
+        .navbar.sticky-top {
+          top: -100px;
+          transition: .5s;
+        }
 
-      <div className="navbar-brand d-flex align-items-center">
-        <a className="navbar-brand ML-1 w-8 sm:w-20" href="#">
-          <img src={logoSF} alt="logo" />
-        </a>
-      </div>
-      <button type="button" className="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarCollapse">
-        <div className="navbar-nav ms-auto p-4 p-lg-0">
-          <a href="" className="nav-item nav-link active text-white" onClick={() => { navigate(`/`); }}>Início</a>
-          <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/todosTurismos`); }}>Turismo</a>
-          <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/todosEventos`); }}>Eventos</a>
-          <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/todasEmpresas`); }}>Empresas</a>
-          <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/todasNoticias`); }}>Noticias</a>
-          <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/login`); }}>Login</a>
+        .navbar {
+          background-color: #064D56 !important; /* Alterando a cor da navbar */
+        }
+
+        .navbar .navbar-nav .nav-link {
+          margin-right: 35px;
+          padding: 25px 0;
+          font-size: 18px;
+          font-weight: normal;
+          outline: none;
+        }
+
+        .navbar .navbar-nav .nav-link:hover,
+        .navbar .navbar-nav .nav-link.active {
+          color: var(--primary);
+        }
+
+        .search-input {
+          display: inline-block;
+          width: 200px;
+          margin-left: 10px;
+        }
+
+        @media (max-width: 991.98px) {
+          .navbar .navbar-nav .nav-link  {
+            margin-right: 0;
+            padding: 10px 0;
+          }
+
+          .search-input {
+            width: calc(100% - 35px);
+            margin-top: 0;
+            margin-left: 0;
+          }
+
+          .border-start {
+            display: flex;
+            align-items: center;
+            width: 100%;
+            padding: 0;
+          }
+
+          .border-start .btn {
+            margin-left: 10px;
+          }
+        }
+        `}
+      </style>
+
+      <nav className="navbar navbar-expand-lg navbar-dark sticky-top px-4 px-lg-5">
+        <div className="navbar-brand d-flex align-items-center">
+          <a className="navbar-brand ML-1 w-8 sm:w-20" href="#">
+            <img src={logoSF} alt="logo" />
+          </a>
         </div>
-        <div className={`border-start ps-4 ${isMobile ? 'd-flex align-items-center' : 'd-none d-lg-block'}`}>
-          {isSearchOpen && (
-            <input type="text" className="form-control search-input" placeholder="Pesquisar..." />
-          )}
-          <button type="button" className="btn btn-sm p-0 ml-2" onClick={handleSearchToggle}>
-            <i className="fa fa-search text-white"></i>
-          </button>
+        <button type="button" className="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className="navbar-nav ms-auto p-4 p-lg-0">
+            <a href="" className="nav-item nav-link active text-white" onClick={() => { navigate(`/`); }}>Início</a>
+            <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/todosTurismos`); }}>Turismo</a>
+            <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/todosEventos`); }}>Eventos</a>
+            <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`#`); }}>Comtur</a>
+            <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/todasNoticias`); }}>Notícias</a>
+            <a href="" className="nav-item nav-link text-white" onClick={() => { navigate(`/login`); }}>Login</a>
+          </div>
+          <div className={`border-start ps-4 ${isMobile ? 'd-flex align-items-center' : 'd-none d-lg-block'}`}>
+            {isSearchOpen && (
+              <input type="text" className="form-control search-input" placeholder="Pesquisar..." />
+            )}
+            <button type="button" className="btn btn-sm p-0 ml-2" onClick={handleSearchToggle}>
+              <i className="fa fa-search text-white"></i> {/* Ícone de pesquisa */}
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 

b79f65472756fcda0dbd81c90bff511f2699b6a0
heyyana <anacconceicao2005@gmail.com>
2024-10-15 10:18:08 -0300

    :sparkles: feat: Adicionando a classe Ata parte 1

---
+++ COMTUR/COMTUR/Models/AtaModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace COMTUR.Models
+{
+	[Table("ata")]
+	public class AtaModel
+	{
+		[Column("idata")]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public Guid Id { get; set; } 
+
+		[Column("tituloata")]
+		public string TituloAta { get; set; }
+
+		[Column("documentoata")]
+		public string DocumentoAta { get; set; }
+
+		[Column("dataata")]
+		public DateOnly DataAta { get; set; }
+	}
+}
---
+++ COMTUR/COMTUR/Repositorios/Interfaces/IAtaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using COMTUR.Models;
+
+namespace COMTUR.Repositorios.Interfaces
+{
+	public interface IAtaRepositorio
+	{
+		Task<IEnumerable<AtaModel>> GetAll();
+		Task<IEnumerable<AtaModel>> GetByProcess(Guid idAta);
+		Task<AtaModel> GetById(Guid id);
+		Task<AtaModel> Create(AtaModel AtaModel);
+		Task<AtaModel> Update(AtaModel AtaModel);
+		Task<AtaModel> Delete(Guid id);
+	}
+}
